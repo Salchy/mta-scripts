0901f6242b820a5adc0e7d1baf7589df024a1854
Salchy <65300209+Salchy@users.noreply.github.com>
2020-05-13 13:03:30 -0300

    Add files via upload

---
+++ SalchyUtil/servidor.lua
@@ -0,0 +1,20 @@
+------------------------------------------------------------------------------------
+--	PROYECTO:			Comunidad Gamers 2.0 (2019)
+--	DERECHOS:			Todos los derechos reservados por los desarrolladores
+--	ARCHIVO:			CGSalchyUtil/servidor.lua
+--	PROPÓSITO:			Sistema para obtener la posicion de markers para futuros sistemas
+--	DESARROLLADORES:	Leandro Correa Gimenez (Salchy)
+------------------------------------------------------------------------------------
+
+function openGUI(player, cmd)
+	local account = getPlayerAccount(player)
+	if (isGuestAccount(account)) then
+		return false
+	end
+	local accname = getAccountName(account)
+	if (not isObjectInACLGroup("user."..accname, aclGetGroup("Admin"))) then
+		return false
+	end
+	triggerClientEvent(player, "CGSalchyUtil.openGUI", player)
+end
+addCommandHandler("markers", openGUI)
